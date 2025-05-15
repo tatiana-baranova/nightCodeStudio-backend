bb5c25a07e635fc152f44fff68a1aaeeb923afd7
tetiana_baranova <syniovych.tanya@gmail.com>
2025-05-15 15:14:37 +0300

    data base

---
+++ src/db/models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true, versionKey: false },
+);
+userSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+};
+
+export const UserCollection = model('user', userSchema);
