994d470d899f02ee8c60f4a4041b174c662dce7a
tetiana_baranova <syniovych.tanya@gmail.com>
2025-05-24 00:05:54 +0300

    update transaction by id

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { createNewTransactionController, getAllTransactionsUserController, deleteTransactionController } from '../controllers/transaction.js';
+import { createNewTransactionController, getAllTransactionsUserController, deleteTransactionController, patchTransactionController } from '../controllers/transaction.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { updateTransactionSchema } from '../validation/transaction.js';
 
 const transactionRouter = Router();
 transactionRouter.post('/', authenticate, ctrlWrapper(createNewTransactionController));
 transactionRouter.get('/', authenticate, ctrlWrapper(getAllTransactionsUserController));
-transactionRouter.delete('/:id', isValidId,authenticate, ctrlWrapper(deleteTransactionController));
+transactionRouter.delete('/:id', isValidId, authenticate, ctrlWrapper(deleteTransactionController));
+transactionRouter.patch('/:id', isValidId, authenticate, validateBody(updateTransactionSchema), ctrlWrapper(patchTransactionController));
 export default transactionRouter;
