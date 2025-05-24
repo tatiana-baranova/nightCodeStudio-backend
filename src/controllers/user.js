ac29d14e3f88092892697d7698b2c45781ef8a2f
Marina.S. <marina.sevostian@gmail.com>
2025-05-24 07:21:47 +0300

    fix photo

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,40 +1,39 @@
-import createHttpError from "http-errors";
+import createHttpError from 'http-errors';
 import { updateUser } from '../services/user.js';
-import { updateUserSchema } from "../validation/user.js";
+import { updateUserSchema } from '../validation/user.js';
 
 export const getCurrentUser = async (req, res) => {
-    if (!req.user) {
-        throw createHttpError(401, 'Not authorized');
-        }
-        res.json({
-            status: 200,
-            message: 'User profile retrieved successfully',
-            data: {
-                id: req.user._id,
-                email: req.user.email,
-                name: req.user.name,
-                balance: req.user.balance,
-            },
-        });
+  if (!req.user) {
+    throw createHttpError(401, 'Not authorized');
+  }
+  res.json({
+    status: 200,
+    message: 'User profile retrieved successfully',
+    data: {
+      id: req.user._id,
+      email: req.user.email,
+      name: req.user.name,
+      balance: req.user.balance,
+      photo: req.user.photo,
+    },
+  });
 };
 
 export const patchUser = async (req, res, next) => {
-    const { error } = updateUserSchema.validate(req.body);
-        if (error) {
-        throw createHttpError(400, 'Username should have at least 2 characters');
-    }
-    const userId = req.user._id;
-    const payload = req.body;
-    const updatedUser = await updateUser(userId, payload);
-        if (!updatedUser) {
-            throw createHttpError(404, 'User not found');
-        }
-    const { password, ...safeUser } = updatedUser.toObject();
-        res.status(200).json({
-            status: 200,
-            message: 'User updated successfully',
-            data: safeUser,
-        });
+  const { error } = updateUserSchema.validate(req.body);
+  if (error) {
+    throw createHttpError(400, 'Username should have at least 2 characters');
+  }
+  const userId = req.user._id;
+  const payload = req.body;
+  const updatedUser = await updateUser(userId, payload);
+  if (!updatedUser) {
+    throw createHttpError(404, 'User not found');
+  }
+  const { password, ...safeUser } = updatedUser.toObject();
+  res.status(200).json({
+    status: 200,
+    message: 'User updated successfully',
+    data: safeUser,
+  });
 };
-
-
